18bf2bb82a4119fc83006e2bf03f27cd67eb6687
jrms <email@fake.com>
2024-03-14 20:47:14 -0700

    Add models to weapons app

--- backend/weapons/models.py
+++ backend/weapons/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Weapon(models.Model):
+    name = models.CharField(max_length=100)
+    damage = models.IntegerField()
+    version = models.CharField(max_length=50)
+
+class Object(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class PowerUp(models.Model):
+    name = models.CharField(max_length=100)
+    effect = models.CharField(max_length=100)
+    duration = models.models.CharField(max_length=100)
+    
