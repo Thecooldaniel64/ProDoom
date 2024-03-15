cbabce358677965082dc31c14e6eba3798285833
jrms <email@fake.com>
2024-03-14 20:51:45 -0700

    Make migrations

--- backend/weapons/models.py
+++ backend/weapons/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-from django.db import models
-
 class Weapon(models.Model):
     name = models.CharField(max_length=100)
     damage = models.IntegerField()
@@ -16,5 +14,5 @@ class Object(models.Model):
 class PowerUp(models.Model):
     name = models.CharField(max_length=100)
     effect = models.CharField(max_length=100)
-    duration = models.models.CharField(max_length=100)
+    duration = models.CharField(max_length=100)
     
